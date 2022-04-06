02190f3cb05bb87513b91b8ae24cf93617ff596e
masanetes <masanetes@gmail.com>
2022-04-06 20:52:33 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://4.bp.blogspot.com/-o8GEdjbc0eA/UUFxneRAUCI/AAAAAAAAOxw/iofcHTC-AJE/s400/animal_dance_rabbit.png" className="App-logo" alt="logo" />
+        <h3>Hello,World! 👋 </h3>
       </header>
     </div>
   );
